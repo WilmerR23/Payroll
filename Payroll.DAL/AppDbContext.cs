bd69ea50b270220cf81b7f7099f2cbbfa89e9e9c
kwilram23 <kwilram23@gmail.com>
2020-05-29 18:29:19 -0400

    carga inicial

---
+++ Payroll.DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Payroll.DAL.Abstractions;
+using Payroll.DAL.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Payroll.DAL
+{
+    public class AppDbContext : DbContext,IDbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<PayrollSheet> PayrollsSheet { get; set; }
+
+        //public Task<int> SaveChangesAsync() => base.SaveChangesAsync();
+        
+        public AppDbContext() : base ("Unapec")
+        {
+        }
+
+    }
+}
---
+++ PayrollNet/Controllers/PayrollController.cs
@@ -0,0 +1,39 @@
+﻿using Payroll.BOL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace PayrollNet.Controllers
+{
+    [RoutePrefix("api/Payroll")]
+    public class PayrollController : ApiController
+    {
+        private FileManagementService _fMS = new FileManagementService();
+
+
+        [HttpGet]
+        [Route("GenPay/{payrollDate}")]
+        public IHttpActionResult GeneratePayrollFile(DateTime payrollDate)
+        {
+            var mensaje  = $"El archivo con el nombre: {_fMS.GenerateOutPutFile(payrollDate)} fue generado.";
+            return Ok(mensaje);
+        }
+
+        [HttpGet]
+        [Route("ReadPay/{fileName}")]
+        public HttpResponseMessage ReadPayrollFile(string fileName)
+        {
+            var data = _fMS.GetOutPutFile(fileName);
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(data, Encoding.UTF8, "text/html")
+            };
+        }
+    }
+}
+
